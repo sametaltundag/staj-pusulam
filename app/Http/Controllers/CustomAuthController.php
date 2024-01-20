4bd677769469ecdfb952fb87f0488cd6d4d540be
Samet ALTUNDAG <smtaltundag63@gmail.com>
2024-01-20 17:06:35 +0300

    User Register ve logout işlemleri tamamlandı.

--- app/Http/Controllers/CustomAuthController.php
+++ app/Http/Controllers/CustomAuthController.php
@@ -4,11 +4,13 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
 
 class CustomAuthController extends Controller
 {
     public function login(Request $request){
-
         $request->validate([
             'email' => 'required|email',
             'password' => 'required'
@@ -18,19 +20,72 @@ class CustomAuthController extends Controller
 
         if ($user) {
             $credentials = $request->only('email', 'password');
-
-            if (auth()->attempt($credentials)) {
-                $userName = explode(' ', $user->name);
-                $userName = $userName[0];
-
-                $request->session()->regenerate();
-
-                return redirect()->route('stajyer-login')->with(['success' => 'Yönlendiriliyorsun...', 'name' => $userName]);
+            if (Auth::attempt($credentials)) {
+                $user = Auth::user();
+                return redirect()->route('dashboard')->with(['success' => 'Hoşgeldin, '.Auth::user()->name]);
             } else {
                 return redirect()->route('stajyer-login')->with('error', 'E-posta ya da şifre hatalı.');
             }
         } else {
             return redirect()->route('stajyer-login')->with('notFound', 'Pusulam kaydınız bulunamadı.');
+        }
+    }
+
+    public function register(Request $request){
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|min:9',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:6',
+            'password2' => 'required|same:password',
+            'gender' => 'required'
+        ], [
+            'name.required' => 'Adınızı giriniz.',
+            'name.min' => 'Adınız en az 5 karakter olmalı.',
+            'email.required' => 'E-posta adresinizi giriniz.',
+            'email.email' => 'Girdiğiniz e-posta adresi geçersiz.',
+            'email.unique' => 'Bu e-posta adresi zaten kayıtlı.',
+            'password.required' => 'Şifrenizi giriniz.',
+            'password.min' => 'Şifreniz en az 6 karakter olmalı.',
+            'password2.required' => 'Şifre tekrarınızı giriniz.',
+            'password2.same' => 'Şifreler uyumlu değil.',
+            'gender.required' => 'Cinsiyetinizi seçiniz.'
+        ]);
+
+        if ($validator->fails()) {
+            return redirect()
+            ->route('stajyer-register')
+            ->withErrors($validator);
+        }
+
+        if($validator){
+            $user = User::create([
+                'name' => $request->name,
+                'email' => $request->email,
+                'password' => Hash::make($request->password),
+                'gender' => $request->gender,
+                'phone' => $request->phone
+            ]);
+
+            if($user){
+                return redirect()->route('stajyer-login')->with('created', 'Artık Pusulan burada!');
+            }
+        }
+    }
+
+
+    public function logout(){
+        if(Auth::check()){
+            Auth::logout();
+            return redirect()->route('stajyer-login')->with('logout', 'Güvenli çıkış yapıldı.');
+        } else {
+            return redirect()->route('home');
+        }
+    }
+    public function dashboard(){
+        if(Auth::check()){
+            return view('frontend.pages.dashboard');
+        } else {
+            return redirect()->route('home');
         }
     }
 }
